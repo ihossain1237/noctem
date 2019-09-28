43a0f24152e9a58cb43c07989b6f57c114e88159
Imran Hossain <ihdev524@gmail.com>
2019-09-28 02:03:52 -0230

    animation

--- app/src/components/cart/cartItem/cartItem.component.jsx
+++ app/src/components/cart/cartItem/cartItem.component.jsx
@@ -3,30 +3,26 @@ import './cartItem.style.scss';
 import Img from 'react-image';
 import {addToCart,removeFromCart,deleteFromCart} from "../../../redux/cart/cart.actions";
 import {connect} from "react-redux";
-import {useSpring,animated} from "react-spring";
 import CustomButton from "../../custom-button/customButton.component";
-import Loader from "../../loader/loader.component";
-import LazyLoad from 'react-lazy-load';
 const CartItem = ({product,addToCart,removeFromCart,deleteFromCart}) => {
-    const props = useSpring({config:{duration:1000},opacity:1,from:{opacity:0}});
     const {name,price,image,itemCount} = product;
     return (
-        <animated.div className={`cart-item`} style={props}>
-            <LazyLoad offsetVertical={0} offsetHorizontal={0}>
-                <Img src={image}  className={`img-fluid cart-item-img`}/>
-            </LazyLoad>
-            <div className={`cart-item-info`}>
-                <span className={`cart-item-info-name`}>{name}</span>
-                <span className={`cart-item-info-price`}>${(price*itemCount).toFixed(2)}</span>
-                <div className={`cart-item-info-quantity`}>
-                    <CustomButton onClick={()=>removeFromCart(product)} > &#10094;</CustomButton>
-                    <span className={`cart-item-info-itemCount`}> {itemCount}</span>
-                    <CustomButton onClick={()=>addToCart(product)} >&#10095;</CustomButton>
+            <div className={`container-fluid cart-item`}>
+                <div>
+                    <Img src={image}  className={`img-fluid cart-item-img`}/>
                 </div>
-                <CustomButton onClick={()=>deleteFromCart(product)} customStyle={`cart-item-remove`} >Delete</CustomButton>
+                <div className={`cart-item-info`}>
+                    <span className={`cart-item-info-name`}>{name}</span>
+                    <span className={`cart-item-info-price`}>${(price*itemCount).toFixed(2)}</span>
+                    <div className={`cart-item-info-quantity`}>
+                        <CustomButton onClick={()=>removeFromCart(product)} > &#10094;</CustomButton>
+                        <span className={`cart-item-info-itemCount`}> {itemCount}</span>
+                        <CustomButton onClick={()=>addToCart(product)} >&#10095;</CustomButton>
+                    </div>
+                    <CustomButton onClick={()=>deleteFromCart(product)} customStyle={`cart-item-remove`} >Delete</CustomButton>
+                </div>
             </div>
 
-        </animated.div>
     );
 };
 
--- app/src/components/productGrid/productGridItem/productGridItem.component.jsx
+++ app/src/components/productGrid/productGridItem/productGridItem.component.jsx
@@ -1,30 +1,34 @@
 import React from 'react';
 
 import './productGridItem.style.scss';
-import 'hover.css'
+
 import CustomButton from "../../custom-button/customButton.component";
 import Img from 'react-image';
 import {addToCart} from "../../../redux/cart/cart.actions";
 import {connect} from "react-redux";
-import {useSpring,animated,config} from "react-spring";
+import {useSpring,animated} from "react-spring";
 import Loader from "../../loader/loader.component";
 import LazyLoad from 'react-lazy-load';
 const ProductGridItem = ({product,addToCart}) => {
-    const props = useSpring({config:{duration:100},opacity:1,from:{opacity:0}});
+
     const {name,image,price}  = product;
-    return     <animated.div  className={`container-fluid productGridItem `} style={props}>
-                <div className={`productGridItem-img`}>
-                    <LazyLoad offsetVertical={0} height={'100%'} width={'100%'}>
-                        <Img className={`img-fluid`} loader={Loader}  src={`${image}`} alt=""/>
-                    </LazyLoad>
+
+    return (
+
+            <div  className={`container-fluid productGridItem `} >
+                <div   className={`productGridItem-img`}>
+                    <Img  className={`img-fluid`}  src={[`${image}`]} loader={Loader} alt=""/>
                 </div>
                 <div className={`productGridItem-info`}>
                     <span className={`productGridItem-info-name`}>{name}</span>
                     <span  className={`productGridItem-info-price`}>${`${price}`}</span>
                     <CustomButton onClick={()=>addToCart(product)} customStyle={`productGridItem-info-btn`} btnText={'Add to cart'}>Add To Cart</CustomButton>
                 </div>
+            </div>
 
-            </animated.div>
+    )
+
+
 
 };
 
